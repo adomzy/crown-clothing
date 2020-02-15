59285eb01bb53854a17e551da0faeae6309603df
obinna <tonyadolalom@gmail.com>
2020-02-15 21:20:57 +0100

    created project with create react app and added files for homepage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomePage from './homepage.component';
+import './homepage.styles.css'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
